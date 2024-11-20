ca04d5b1dc967b46d4bc6adc33c15b1ca15eda0e
Erio <Erio@DESKTOP-BENFUVG.mshome.net>
2024-11-20 16:30:48 -0300

    Entrega da atividade 10

---
+++ prg03Rafael20232/src/br/com/ifba/atividade10/vew/Publicacao.java
@@ -0,0 +1,10 @@
+
+package br.com.ifba.atividade10.vew;
+
+public interface Publicacao {
+    public void abrir();
+    public void fechar();
+    public void folhear(int p);
+    public void avancarPag();
+    public void voltarPag();
+}
